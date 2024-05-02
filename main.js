8e74f7aee0f4d22f21893b07dbe2727690235b60
Leonardo Lima <leonardolima453@gmail.com>
2024-05-02 11:12:47 -0300

    Primeira parte

---
+++ main.js
@@ -0,0 +1,5 @@
+let carro = document.getElementById("carro").value;
+let ano = document.getElementById("ano").value;
+
+document.getElementById("carro").value = "";
+document.getElementById("ano").value = "";
