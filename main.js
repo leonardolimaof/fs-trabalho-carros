5e8b0e50dd7452039fad510fe8728574de75462f
Leonardo Lima <leonardolima453@gmail.com>
2024-05-04 16:47:41 -0300

    botao delete funcionando

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+})
+
+
 document.getElementById('enviar').addEventListener('click', function() {
     const carro = document.getElementById('carro').value
     const ano = document.getElementById('ano').value
@@ -43,17 +47,25 @@ document.getElementById('enviar').addEventListener('click', function() {
                         <td>${item.id}</td>
                         <td>${item.carro}</td>
                         <td>${item.ano}</td>
-                        <button id="editar" class="btn btn-primary btn-sm m-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-                      </svg> Editar</button><button id="deletar" class="btn btn-danger btn-sm"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                    </svg> Deletar</button>
+                        <td>
+                        <button class="btn btn-primary btn-sm m-2" onclick="editar"><i class="bi bi-pencil-square"></i> Editar</button>
+                      <button class="btn btn-danger btn-sm" onclick="deletar('${item.id}')"><i class="bi bi-trash"></i> Deletar</button>
+                        </td>
                     `;
                     tbody.appendChild(row);
                 });
             })
             .catch(error => console.error('Erro ao carregar o JSON:', error));
 
-            
+
+            function deletar(id) {
+                fetch(`http://localhost:3000/Veiculos/${id}`, {
+                    method: 'DELETE'
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Dados deletados com sucesso:', data);
+                    location.reload();
+                })
+                .catch(error => console.error('Erro ao deletar o JSON:', error));
+            }
