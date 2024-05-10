868bb3084fa2e86da03e7debbd839f15ca7190af
Leonardo Lima <leonardolima453@gmail.com>
2024-05-10 00:23:31 -0300

    Finalizado

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ document.getElementById('enviar').addEventListener('click', function(event) {
     event.preventDefault(); // Evita o comportamento padrão do formulário de enviar
 
     const carro = document.getElementById('carro').value;
+    const modelo = document.getElementById('modelo').value;
+    const cor = document.getElementById('cor').value;
     const ano = document.getElementById('ano').value;
 
     document.getElementById('carro').value = "";
+    document.getElementById('modelo').value = "";
+    document.getElementById('cor').value = "";
     document.getElementById('ano').value = "";
 
     const data = {
         carro: carro,
+        modelo: modelo,
+        cor: cor,
         ano: ano
     };
 
@@ -31,12 +37,7 @@ document.getElementById('enviar').addEventListener('click', function(event) {
         // Fecha o modal após a inserção dos dados
         $('#modalEditar').modal('hide');
 
-        // Recarrega a lista de veículos
-        // Substitua esta linha pela função que recarrega a lista de veículos no seu contexto
-        // Por exemplo, se você estiver usando Vue.js, pode chamar um método para recarregar a lista de veículos
-        // Se você estiver usando uma biblioteca de manipulação do DOM, pode atualizar a lista diretamente
-        // Se precisar de ajuda com isso, por favor, me avise
-        // Por enquanto, vamos apenas recarregar a página inteira
+        
         window.location.reload();
     })
     .catch((error) => {
@@ -57,6 +58,8 @@ document.getElementById('enviar').addEventListener('click', function(event) {
             row.innerHTML = `
                 <td>${item.id}</td>
                 <td>${item.carro}</td>
+                <td>${item.modelo}</td>
+                <td>${item.cor}</td>
                 <td>${item.ano}</td>
                 <td>
                     <button class="btn btn-primary" href="#" data-toggle="modal" data-target="#modalEditar_${item.id}" onclick="editar('${item.id}')">
@@ -73,18 +76,22 @@ document.getElementById('enviar').addEventListener('click', function(event) {
                                 </div>
                                 <div class="modal-body">
                                     <div class="container text-center">
-                                    <p>Veiculo de ID: ${item.id}</p>
-                                        <div class="row text-center justify-content-center m-3">
-                                            <form id="form_${item.id}">
-                                                <div class="form-group">
-                                                    <input type="text" class="form-control" id="carro_${item.id}" name="carro" placeholder="Nome do carro">
-                                                </div>
-                                                <div class="form-group">
-                                                    <input type="number" class="form-control" id="ano_${item.id}" name="ano" placeholder="Ano de fabricação">
-                                                </div>
-                                                <button type="button" class="btn btn-primary m-3" onclick="atualizar('${item.id}')"><i class="bi bi-arrow-down-up"></i> Atualizar</button>
-                                            </form>
-                                        </div>
+                                        <p>Veículo de ID: ${item.id}</p>
+                                        <form>
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" id="carro_${item.id}" name="carro" value="${item.carro}" placeholder="Veículo">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" id="modelo_${item.id}" name="modelo" value="${item.modelo}" placeholder="Modelo">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="color" class="form-control" id="cor_${item.id}" name="cor" value="${item.cor}" placeholder="cor">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="number" class="form-control" id="ano_${item.id}" name="ano" value="${item.ano}" placeholder="Ano de fabricação">
+                                            </div>
+                                            <button type="button" class="btn btn-primary m-3" onclick="atualizar('${item.id}')"><i class="bi bi-arrow-down-up"></i> Atualizar</button>
+                                        </form>
                                     </div>
                                 </div>
                                 <div class="modal-footer">
@@ -103,46 +110,85 @@ document.getElementById('enviar').addEventListener('click', function(event) {
 
 
 
-            function deletar(id) {
+    function deletar(id) {
+        Swal.fire({
+            title: 'Tem certeza?',
+            text: 'Deseja realmente deletar os dados?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Sim, deletar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
                 fetch(`http://localhost:3000/Veiculos/${id}`, {
                     method: 'DELETE'
                 })
                 .then(response => response.json())
                 .then(data => {
                     console.log('Dados deletados com sucesso:', data);
-                    location.reload();
+                    Swal.fire({
+                        title: 'Deletado com sucesso!',
+                        text: 'Os dados foram deletados com sucesso.',
+                        icon: 'success'
+                    }).then(() => {
+                        location.reload();
+                    });
                 })
                 .catch(error => console.error('Erro ao deletar o JSON:', error));
             }
-
+        });
+    }
+    
           
             
-            function editar(id, carro, ano) {
+            function editar(id, carro, modelo, cor, ano) {
                 document.getElementById(`carro`).value = carro;
+                document.getElementById(`modelo`).value = modelo;
+                document.getElementById(`cor`).value = cor;
                 document.getElementById(`ano`).value = ano;
             }
-
+            
             function atualizar(id) {
-                const carro = document.getElementById(`carro_${id}`).value
-                const ano = document.getElementById(`ano_${id}`).value
-
-                fetch(`http://localhost:3000/Veiculos/${id}`, {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({carro: carro, ano: ano}),
-                })
-
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Não foi possível atualizar o JSON');
+                const carro = document.getElementById(`carro_${id}`).value;
+                const modelo = document.getElementById(`modelo_${id}`).value;
+                const cor = document.getElementById(`cor_${id}`).value;
+                const ano = document.getElementById(`ano_${id}`).value;
+            
+                Swal.fire({
+                    title: 'Tem certeza?',
+                    text: 'Deseja realmente atualizar os dados?',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Sim, atualizar!',
+                    cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // Chama a função editar para atualizar os campos de carro e ano
+                        editar(id, carro, ano);
+            
+                        fetch(`http://localhost:3000/Veiculos/${id}`, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({ carro: carro, modelo: modelo, cor: cor, ano: ano }),
+                        })
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error('Não foi possível atualizar o JSON');
+                            }
+                            return response.json();
+                        })
+                        .then(data => {
+                            console.log('Dados atualizados com sucesso:', data);
+                            location.reload();
+                        })
+                        .catch(error => console.error('Erro ao atualizar o JSON:', error));
                     }
-                    return response.json();
-                })
-                .then(data => {
-                    console.log('Dados atualizados com sucesso:', data);
-                    location.reload();
-                })
-                .catch(error => console.error('Erro ao atualizar o JSON:', error));
+                });
             }
+            
